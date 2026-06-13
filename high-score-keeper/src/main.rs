56abd5eae6505a8e06dd56beec0852f2fa49ecab
Daniel Lay <Daniellay.work@hotmail.com>
2026-06-13 22:08:32 +1000

    implementing a score tracking app to understand file read & write

---
+++ high-score-keeper/src/main.rs
@@ -0,0 +1,52 @@
+use std::fs;
+use rand::Rng;
+use std::cmp::Ordering; 
+use std::io::{ stdin, stdout, Write };
+
+fn play_guessing_game() -> u32 {
+    let mut tries: u32 = 15;
+    let secret_number = rand::thread_rng().gen_range(0..=100);
+    while tries > 0 {
+        let mut guess: String = String::new();
+        print!("\nPlease Enter a number: ");
+        let _ = stdout().flush();
+        stdin().read_line(&mut guess).expect("This should work");
+        let guess: u32 = match guess.trim().parse() {
+                        Ok(num) => num,
+                        Err(_) => {
+                            println!("\n!!! Please provide a number !!!");
+                            continue;
+                        }
+                    };
+            
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("\n Too Small!!"),
+            Ordering::Greater => println!("\n Too Big!!"),
+            Ordering::Equal => {
+                println!("\nYou Won!!!"); 
+                break 
+            }
+        };
+        tries -= 1;
+    }
+    return tries;
+}
+
+
+
+fn main() {
+    let filename: &str = "score.txt";
+    let data = fs::read_to_string(filename)
+                .unwrap_or("0".to_string())
+                .parse::<u32>()
+                .unwrap_or(0);
+    let score: u32 = play_guessing_game(); 
+    
+    if  score > data { 
+        fs::write(filename, score.to_string()).expect("Write failed");
+        println!("New High Score!");
+    }
+    
+    
+}
+    
---
+++ todolist/src/main.rs
@@ -0,0 +1,20 @@
+use std::io::{ Write, stdin, stdout};
+
+
+fn add_to_list() {
+}
+
+fn print_list() {
+}
+
+fn complete_item_in_list() {
+}
+
+// Optional
+
+// fn save_state() { }
+// fn load_state() { }
+
+fn main() {
+
+}
